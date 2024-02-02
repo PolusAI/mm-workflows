8824482342f4f874f5c430af6498323c1ee1c1aa
Nazanin Donyapour <nazanin.donyapour@gmail.com>
2024-02-02 13:59:05 -0500

    fix error logging issue

--- examples/scripts/obminimize.sh
+++ examples/scripts/obminimize.sh
@@ -3,4 +3,4 @@
 # convergence on the second timestep. (same with -newton)
 # Use steepest descent because it's the only one that works!
 obminimize -sd -o mol2 "$1" > temp.mol2 # -xu is ignored here
-obabel temp.mol2 -o mol2 -xu
+obabel temp.mol2 -o mol2 -O "$2" -xu
